d28c93523a9bbc4f6144c5041a5d00f2376326ac
Anastasiia Komleva <asyakoma1998@gmail.com>
2024-03-05 00:18:48 +0300

    подключает скрипты для видео

--- source/js/main.js
+++ source/js/main.js
@@ -1,2 +1,12 @@
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+import {findVideos} from './modules/video';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  findVideos();
+
+  window.addEventListener('load', () => {
+
+  });
+});
